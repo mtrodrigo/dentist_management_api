4715032fbde5d0766ed417f45ace292731c940d4
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-04-14 17:10:14 -0300

    cria: método para atualizar paciente e adiciona rota correspondente

--- src/controller/PatientsController.js
+++ src/controller/PatientsController.js
@@ -61,7 +61,7 @@ export default class PatientsController {
       state,
       zipCode,
       medicalHistory,
-      user: {_id: user._id},
+      user: { _id: user._id },
     });
     try {
       const newPatient = await patient.save();
@@ -75,21 +75,98 @@ export default class PatientsController {
   }
 
   static async getAllUserPatients(req, res) {
-    const token = getToken(req)
-    const user = await getUserByToken(token)
+    const token = getToken(req);
+    const user = await getUserByToken(token);
 
-    const patients = await Patient.find({"user._id": user._id}).sort("-createdAt")
-    res.status(200).json({ patients })
+    const patients = await Patient.find({ "user._id": user._id }).sort(
+      "-createdAt"
+    );
+    res.status(200).json({ patients });
   }
 
   static async getPatientById(req, res) {
-    const id = req.params.id
-    const token = getToken(req)
-    const user = await getUserByToken(token)
-    const patient = await Patient.findOne({ _id: id, "user._id": user._id })
+    const id = req.params.id;
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+    const patient = await Patient.findOne({ _id: id, "user._id": user._id });
     if (!patient) {
       return res.status(404).json({ message: "Paciente não encontrado" });
     }
-    res.status(200).json({ patient })
+    res.status(200).json({ patient });
+  }
+
+  static async updatePatient(req, res) {
+    const id = req.params.id;
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (!req.body) {
+      return res
+        .status(400)
+        .json({ message: "Campo obrigatório não preenchido" });
+    }
+
+    const {
+      name,
+      email,
+      phone,
+      cpf,
+      birthDate,
+      address,
+      city,
+      state,
+      zipCode,
+      medicalHistory,
+    } = req.body;
+
+    //validation
+    if (
+      !name ||
+      !email ||
+      !phone ||
+      !cpf ||
+      !birthDate ||
+      !address ||
+      !city ||
+      !state ||
+      !zipCode ||
+      !medicalHistory
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Campo obrigatório não preenchido" });
+    }
+
+    //check if the user is authenticated
+    const patient = await Patient.findOne({ _id: id, "user._id": user._id });
+    if (!patient) {
+      return res.status(404).json({ message: "Paciente não encontrado" });
+    }
+
+    //update the patient
+    try {
+      await Patient.updateOne(
+        { _id: id },
+        {
+          $set: {
+            name,
+            email,
+            phone,
+            cpf,
+            birthDate,
+            address,
+            city,
+            state,
+            zipCode,
+            medicalHistory,
+          },
+        }
+      );
+      return res
+        .status(200)
+        .json({ message: "Paciente atualizado com sucesso" });
+    } catch (error) {
+      return res.status(500).json({ message: "Erro ao atualizar paciente" });
+    }
   }
 }
--- src/routes/PatientsRouter.js
+++ src/routes/PatientsRouter.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/create', checkToken, PatientsController.create);
 router.get('/mypatients', checkToken, PatientsController.getAllUserPatients)
 router.get('/mypatients/:id', checkToken, PatientsController.getPatientById)
+router.patch('/mypatients/:id', checkToken, PatientsController.updatePatient)
 
 export default router;
