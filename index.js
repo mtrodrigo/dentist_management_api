b864932b4f44ab77fb833838f818fd29470ab0bd
Rodrigo Marques Tavares <157378101+mtrodrigo@users.noreply.github.com>
2025-06-24 15:21:38 -0300

    fix: add metodo patch no cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: ['https://dentist-management-nine.vercel.app', 'http://localhost:5173'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
